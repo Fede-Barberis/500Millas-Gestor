d0886f7762af4a34b3f685d3d29ee10dab1f8d02
Federico Barberis <fedeandresbarberis@gmail.com>
2025-11-05 11:09:36 -0300

    feat: agregar layout de autenticación con fondos dinámicos

---
+++ frontend/src/layouts/AuthLayout.jsx
@@ -0,0 +1,33 @@
+import { Outlet, useLocation } from "react-router-dom"
+import { Toaster } from "sonner"
+
+export default function AuthLayout() {
+    const location = useLocation();
+
+    const isLogin = location.pathname.includes("login");
+    const isRegister = location.pathname.includes("register");
+
+    const backgroundStyle = {
+        backgroundImage: isLogin
+        ? "url('/src/assets/fondoLogin.svg')"
+        : "url('/src/assets/fondoRegister.svg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    };
+
+    return (
+        <>
+            <div className="bg-cover bg-center"
+            style={backgroundStyle}>
+                <div className="max-w-lg mx-auto px-5">
+                    <div>
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+
+            <Toaster position="bottom-right"/>
+
+        </>
+    )
+}
