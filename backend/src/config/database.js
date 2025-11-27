7d288fe89da70d1dd58a9bb832c6849116866f66
Federico Barberis <fedeandresbarberis@gmail.com>
2025-11-27 16:58:38 -0300

    feat: agregar opciones de dialecto y puerto a la configuración de Sequelize

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,6 +7,10 @@ dotenv.config()
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: false
+    },
     logging: false,
 });
 
