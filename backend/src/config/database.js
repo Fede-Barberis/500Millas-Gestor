52c8daea540ba76f7c6ee6c605cd6fb032d0b74a
Federico Barberis <fedeandresbarberis@gmail.com>
2025-11-27 17:36:15 -0300

    feat: ajustar configuración del pool de conexiones en Sequelize

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -15,9 +15,9 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         } : false
     },
     pool: {
-        max: 5,
+        max: 2,
         min: 0,
-        acquire: 30000,
+        acquire: 20000,
         idle: 10000
     },
     logging: process.env.NODE_ENV !== 'production',
